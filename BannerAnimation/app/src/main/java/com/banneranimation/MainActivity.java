1369a42b297474933999ff1aa6a3ae1a06d77103
supermanmwg <554607289@qq.com>
2015-12-31 10:27:24 +0800

    animation swing

---
+++ BannerAnimation/app/src/main/java/com/banneranimation/MainActivity.java
@@ -0,0 +1,48 @@
+package com.banneranimation;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.OvershootInterpolator;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView helloTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        helloTv = (TextView) findViewById(R.id.hello);
+
+        final ObjectAnimator down = new ObjectAnimator().ofFloat(helloTv, "rotation", 2);
+        down.setDuration(100);
+        down.setInterpolator(new AccelerateInterpolator());
+
+
+        final ObjectAnimator up = new ObjectAnimator().ofFloat(helloTv, "rotation", -2);
+        up.setDuration(100);
+        up.setInterpolator(new AccelerateInterpolator());
+
+        final AnimatorSet bounce = new AnimatorSet();
+        bounce.setStartDelay(1000);
+        bounce.playSequentially(down, up);
+
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                bounce.start();
+                Log.d("haha", "bounce is starting!");
+                handler.postDelayed(this, 200);
+            }
+        };
+        handler.post(runnable);
+    }
+}
